5e6fa10575bc0e61ad232bb09bd903211d4f1c76
yastamardika <rattyasta1@gmail.com>
2022-03-10 04:33:15 +0700

    change seeder url

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,7 +2,7 @@ pages << {
     page_type: "search",
     method: "GET",
     headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-    url: "https://www.tokopedia.com/search?q=monitor%20samsung&rf=true&srp_component_id=03.08.01.00",
+    url: "https://www.tokopedia.com/search?q=monitor%20samsung",
     vars: {
       category: "LCD Monitor"
     },
