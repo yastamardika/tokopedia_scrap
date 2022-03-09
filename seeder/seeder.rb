24eb8de79db0420d6af3592fe8f2cde918fd6c22
yastamardika <rattyasta1@gmail.com>
2022-03-10 03:34:25 +0700

    add config and seeder

---
+++ seeder/seeder.rb
@@ -0,0 +1,10 @@
+pages << {
+    page_type: "search",
+    method: "GET",
+    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    url: "https://www.tokopedia.com/search?q=monitor%20samsung&rf=true&srp_component_id=03.08.01.00",
+    vars: {
+      category: "LCD Monitor"
+    },
+    fetch_type: "browser"
+}
